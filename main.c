9a108815984e72112ad984112f348cb7ffdb2b0f
dkalaxdk <alax0013@gmail.com>
2018-11-27 22:29:26 +0100

    Changed lines_counter to a string_counter, as well as beginning work on cleaner function

--- main.c
+++ main.c
@@ -3,16 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 #define MAX_NAME_SIZE 30
+#define MAXIMUM_FILE_LINES 2000
+#define MAXIMUM_RACERS 300
 
 enum race_locations {ParisRiybaix=0,AmstelGoldRace=1,LaFlecheWallonne=2,LiegeBastogneLiege=3};
-
-
-struct  race_stats{
-    int                 races_locations[LiegeBastogneLiege][];
-    int                 points;
-    int                 participants;
-
-} race_stats;
 
 struct racer {
     char                biker_first_name[MAX_NAME_SIZE];
@@ -22,8 +16,19 @@ struct racer {
     char                biker_nation[4];
     char                biker_position[4];
     char                bike_time[10];
-    struct race_stats   race_stats;
+    char                position[4];
+    int                 points;
+    char                race_time[10];
 } racer;
+
+struct  race_stats{
+    int                 races_locations;
+    int                 number_of_participants;
+    struct racer        participants[MAXIMUM_RACERS];
+
+} race_stats;
+
+
 
 struct input_lines {
     char                race_name[MAX_NAME_SIZE];
@@ -38,37 +43,55 @@ struct input_lines {
 
 
 void parser(struct input_lines current_result[]);
-int lines_counter();
-struct input_lines point_counter(struct input_lines result,const int lines);
+int string_counter(char *search_string);
 void itallian_bikers(struct input_lines result[]);
 char danish_bikers(struct input_lines result[]);
-int danes_counter(struct input_lines result[]);
-void cleaner();
+void cleaner(struct input_lines input[], struct race_stats output[]);
 
 
 int main() {
     struct input_lines *results_array;
-    results_array = (struct input_lines *) calloc (lines_counter(), sizeof(struct input_lines));
-
+    struct race_stats *races_array;
+    int lines;
+    char input[2] = "\n";
+    lines = string_counter(input);
+    races_array =  (struct race_stats *)        calloc (4, sizeof(struct race_stats));
+    results_array = (struct input_lines *)  calloc (lines, sizeof(struct input_lines));
     /*Populating array of results*/
+
     parser(results_array);
     /*printf("Result test: %s \n",results_array[i].biker_first_name);*/
-    cleaner(results_array,);
-    itallian_bikers(results_array);
-    danish_bikers(results_array);
+    cleaner(results_array,races_array);
+    //itallian_bikers(results_array);
+    //danish_bikers(results_array);
 
     /*point_counter(results_array,lines);*/
 
-    free(results_array);
+    //free(results_array);
     return 0;
+}
+
+int string_counter(char *search_string){
+    FILE *file;
+    file = fopen("cykelloeb.txt","r");
+    int n = 0;
+    char line[MAXIMUM_FILE_LINES];
+
+    while (fgets(line, MAXIMUM_FILE_LINES, file) != NULL){
+        if (strstr(line, search_string) != NULL){
+            n++;
+        }
+    }
+    fclose(file);
+    return n;
 }
 
 void parser(struct input_lines current_result[]) {
     FILE *file;
     file = fopen("cykelloeb.txt","r");
-    int i,j,lines;
-    lines=lines_counter();
-    for (i = 0; i < lines(); ++i) {
+    int i,lines;
+    lines=string_counter("\n");
+    for (i = 0; i < lines; ++i) {
         /*ScanF for basic information*/
         fscanf(file, " %[a-zA-Z] "
                      " \"%[a-zA-Z-] "
@@ -82,44 +105,48 @@ void parser(struct input_lines current_result[]) {
                &current_result[i].biker_age, current_result[i].biker_team, current_result[i].biker_nation,
                current_result[i].biker_position, current_result[i].bike_time);
         /*Handle middlenames*/
-        j= strlen(current_result.biker_last_name[i]);
-        while (!islower(current_result[i].biker_last_name[j])) {
-            j--;
-        }
+        //j=strlen(current_result[i].biker_last_name);
+        //while (!islower(current_result[i].biker_last_name[j])) {
+        //    j--;
+        //}
     }
     fclose(file);
 }
-void cleaner() {
+void cleaner(struct input_lines input[], struct race_stats output[]) {
+    int ParisRoubaoix_participants,AmstelGoldRace_participants,LaFlecheWallonne_participants,LiegeBastogneLiege_participants;
+    int i,j;
+    ParisRoubaoix_participants =        string_counter("ParisRoubaix");
+    AmstelGoldRace_participants =       string_counter("AmstelGoldRace");
+    LaFlecheWallonne_participants =     string_counter("LaFlecheWallonne");
+    LiegeBastogneLiege_participants =   string_counter("LiegeBastogneLiege");
 
-}
-
-int lines_counter() {
-    FILE *file;
-    file = fopen("cykelloeb.txt","r");
-    int lines=0;
-    char test;
-    while (!feof(file)) {
-        test = fgetc(file);
-        if (test=='\n') {
-            lines ++;
+    /*
+     * Hvert ræs skal have en mængde af associerede køre, samt antallet af køre i ræset.
+     *
+     */
+    output[ParisRiybaix].number_of_participants = ParisRoubaoix_participants;
+    output[ParisRiybaix].races_locations = ParisRiybaix;
+    for ( i = 0; i < string_counter("\n"); ++i) {
+        if (i < ParisRoubaoix_participants) {
+            strcpy(output->participants[i].biker_first_name, input[i].biker_first_name);
+            strcpy(output->participants[i].biker_last_name, input[i].biker_last_name);
+            strcpy(output->participants[i].biker_first_name, input[i].biker_first_name);
+            strcpy(output->participants[i].biker_position, input[i].biker_position);
+            strcpy(output->participants[i].biker_nation, input[i].biker_nation);
+            strcpy(output->participants[i].biker_team, input[i].biker_team);
+            strcpy(output->participants[i].race_time, input[i].bike_time);
+            output->participants[i].biker_age = input[i].biker_age;
         }
     }
-    fclose(file);
-    return lines;
+
+
+
 }
-
-/*struct input_lines point_counter(struct input_lines *result,const int lines) {
-    int i;
-    for (i = 0; i < lines; ++i) {
-
-    }
-    return *result;
-}*/
 
 void itallian_bikers(struct input_lines result[]) {
     int i;
     printf("Itallian bikers and their times: \n \n");
-    for (i = 0; i <lines_counter(); ++i) {
+    for (i = 0; i < string_counter("\n"); ++i) {
         if (strcmp(result[i].biker_nation,"ITA") == 0 && result[i].biker_age > 30) {
                 printf("The biker: %s %s | %s | %s |\n",result[i].biker_first_name,result[i].biker_last_name,result[i].race_name,result[i].bike_time);
         }
@@ -130,15 +157,7 @@ char danish_bikers(struct input_lines result[]) {
 
 }
 
-int string_counter(struct input_lines result[]) {
-    int i,danes_counter=0;
-    for (i = 0; i < lines_counter(); ++i) {
-        if (strcmp(result[i].biker_nation,"DEN")==0) {
-            danes_counter++;
-        }
-    }
-    return danes_counter;
-}
+
 
 void top_ten() {
 
